38e0ce0221f7e6a714d81c89a594ab1e227848b1
redouane <redouane.sobaihi@live.com>
2022-11-09 00:27:18 +0300

    add AddWallet methods

---
+++ fall_project_2/User.cs
@@ -0,0 +1,26 @@
+
+using fall_project_2.enums;
+
+namespace fall_project_2;
+
+public class User
+{
+    public string Name { get; }
+
+    public string Email { get; }
+
+    public List<Wallet> Wallets { get; }
+
+    public void AddWallet(Wallet wallet)
+    {
+        // TODO: check if user does not own wallet with the provided wallet currency 
+        // TODO: add wallet to wallets list
+    }
+
+    public void AddWallet(Currency currency)
+    {
+        // TODO: check if user does not own wallet with the provided currency 
+        // TODO: create wallet instance
+        // TODO: add wallet to wallets list
+    }
+}
