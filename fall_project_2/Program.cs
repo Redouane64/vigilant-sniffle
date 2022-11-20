8efc80aec22885e33a2e2776afd87c8ec572c32e
redouane <redouane.sobaihi@live.com>
2022-11-20 18:41:55 +0300

    add program loops

--- fall_project_2/Program.cs
+++ fall_project_2/Program.cs
@@ -1,91 +1,196 @@
-﻿Console.Clear();
-Console.WriteLine("Welcome to your personal finance console app");
-Console.WriteLine("Choose an option");
-Console.WriteLine("[1] register");
-Console.WriteLine("[2] login");
-Console.WriteLine("[3] exit");
-Console.Write($"{Environment.NewLine}Select an option: ");
-
-switch (Console.ReadLine())
+internal class Program
 {
-    case "1":
-        Console.WriteLine("This is the register option");
-        break;
-    case "2":
-        Console.WriteLine("This is the login option");
-        break;
-    case "3":
-        Console.WriteLine("GoodBye!");
-        break;
-}
+    static bool terminateProgram = false;
+    static bool isAuthenticated = false;
 
-static (String currency, String amount) CreateWallet()
-{
-    Console.Write("Please choose currency");
-    var currency = Console.ReadLine();
-    Console.Write("Please enter your amount");
-    var amount = Console.ReadLine();
-
-    return (currency, amount);
-}
-
-static (String email, String password) Login()
-{
-    Console.Write("Please enter your email");
-    var email = Console.ReadLine();
-    Console.Write("Please enter your password");
-    var password = ReadPassword();
-    return (email, password);
-}
-
-static (String name, String email, String password) Register()
-{
-    Console.Write("Please enter your name");
-    var name = Console.ReadLine();
-    Console.Write("Please enter your email");
-    var email = Console.ReadLine();
-    Console.Write("Please enter your password");
-    var password = ReadPassword();
-
-    return (name, email, password);
-}
-
-// Helper to read masked password
-static string ReadPassword()
-{
-    var value = new List<char>();
-    while (true)
+    private static void Main(string[] args)
     {
-        var key = Console.ReadKey(true);
-        Console.Write('*');
 
-        if (key.Key == ConsoleKey.Enter)
+        do
         {
-            break;
+            // red program loop
+            Console.WriteLine("Welcome to your personal finance console app");
+            Console.WriteLine("Choose an option");
+            Console.WriteLine("[1] register");
+            Console.WriteLine("[2] login");
+            Console.WriteLine("[3] exit");
+            Console.Write($"{Environment.NewLine}Select an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+
+                    do
+                    {
+                        // green program loop
+
+                        try
+                        {
+                            var input = Register();
+                            // TODO: 1. create user object
+                            // TODO: 2. save user object to database
+                            // TODO: 3. move to logout/create/delete/wallet
+
+                            isAuthenticated = true;
+                            ShowMainMenu();
+                        }
+                        catch (Exception exception)
+                        {
+                            Console.Error.WriteLine("Unable to register");
+                        }
+
+                        if (!isAuthenticated)
+                        {
+                            break;
+                        }
+
+
+                    } while (true);
+
+                    break;
+
+                case "2":
+
+                    // green program loop
+                    do
+                    {
+                        try
+                        {
+                            var input = Login();
+                            // TODO: 1. verify user credentials
+                            // TODO: 2. move to logout/create/delete/wallet
+
+                            isAuthenticated = true;
+                            ShowMainMenu();
+
+                        }
+                        catch (Exception exception)
+                        {
+                            Console.Error.WriteLine("Unable to login");
+                        }
+
+                        if (!isAuthenticated)
+                        {
+                            break;
+                        }
+
+                    } while (true);
+
+                    break;
+                case "3":
+                    Console.WriteLine("GoodBye!");
+                    terminateProgram = true;
+                    break;
+            }
+        } while (!terminateProgram);
+
+        static (string currency, string amount) CreateWallet()
+        {
+            Console.Write("Please choose currency: ");
+            var currency = Console.ReadLine();
+            Console.Write("Please enter your amount: ");
+            var amount = Console.ReadLine();
+
+            return (currency, amount);
         }
 
-        value.Add(key.KeyChar);
+        static (string email, string password) Login()
+        {
+            Console.Write("Please enter your email: ");
+            var email = Console.ReadLine();
+            Console.Write("Please enter your password: ");
+            var password = ReadPassword();
+            return (email, password);
+        }
+
+        static (string name, string email, string password) Register()
+        {
+            Console.Write("Please enter your name: ");
+            var name = Console.ReadLine();
+            Console.Write("Please enter your email: ");
+            var email = Console.ReadLine();
+            Console.Write("Please enter your password: ");
+            var password = ReadPassword();
+
+            return (name, email, password);
+        }
+
+        // Helper to read masked password
+        static string ReadPassword()
+        {
+            var value = new List<char>();
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+                Console.Write('*');
+
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+
+                value.Add(key.KeyChar);
+            }
+
+            return new string(value.ToArray());
+        }
+
+        static (string amount, string date, string category) AddOperation()
+        {
+            Console.Write("Please enter your amount: ");
+            var amount = Console.ReadLine();
+            Console.Write("Please enter your date: ");
+            var date = Console.ReadLine();
+            Console.Write("Choose your category: ");
+            var category = Console.ReadLine();
+
+            return (amount, date, category);
+        }
+
+        static string ChooseWallet()
+        {
+            Console.Write("Please enter your wallet name: ");
+            var name = Console.ReadLine();
+
+            return name;
+        }
+
+        static void ShowMainMenu()
+        {
+            Console.WriteLine("Choose an option");
+            Console.WriteLine("[1] Create Wallet");
+            Console.WriteLine("[2] Choose Wallet");
+            Console.WriteLine("[3] Delete Wallet");
+            Console.WriteLine("[4] Logout");
+            Console.WriteLine();
+            Console.Write("Select an option: ");
+            // orange loop
+            do
+            {
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.WriteLine("Create Wallet Menu Selected");
+
+                        break;
+                    case "2":
+                        Console.WriteLine("Choose Wallet Menu Selected");
+
+                        break;
+                    case "3":
+                        Console.WriteLine("Delete Wallet Menu Selected");
+
+                        break;
+
+                    case "4":
+                        isAuthenticated = false;
+
+                        Console.WriteLine("Logged out successfully!");
+                        break;
+                }
+
+            } while (isAuthenticated);
+        }
     }
-
-    return new String(value.ToArray());
-}
-
-static (String amount, String date, String category) AddOperation()
-{
-    Console.Write("Please enter your amount");
-    var amount = Console.ReadLine();
-    Console.Write("Please enter your date");
-    var date = Console.ReadLine();
-    Console.Write("Choose your category");
-    var category = Console.ReadLine();
-
-    return (amount, date, category);
-}
-
-static String ChooseWallet()
-{
-    Console.Write("Please enter your wallet name");
-    var name = Console.ReadLine();
-
-    return name;
 }
