40bfa486d1abddea9ec8fdc358ba07db1569cc7c
redouane <redouane.sobaihi@live.com>
2022-12-04 19:29:23 +0300

    Add wallet menus and Storage functions

--- fall_project_2/Program.cs
+++ fall_project_2/Program.cs
@@ -1,4 +1,8 @@
+﻿
 using System.Text.RegularExpressions;
+
+using fall_project_2;
+using fall_project_2.Data;
 
 internal class Program
 {
@@ -94,166 +98,238 @@ internal class Program
                     break;
             }
         } while (!terminateProgram);
+    }
 
-        static (string currency, string amount) CreateWallet()
+    static (string currency, string amount) CreateWallet()
+    {
+        Console.Write("Please choose currency: ");
+        var currency = Console.ReadLine();
+        Console.Write("Please enter your amount: ");
+        var amount = Console.ReadLine();
+
+        return (currency, amount);
+    }
+
+    static (string email, string password) Login()
+    {
+        Console.Write("Please enter your email: ");
+        var email = Console.ReadLine();
+        if (!Regex.IsMatch(email, "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$"))
         {
-            Console.Write("Please choose currency: ");
-            var currency = Console.ReadLine();
-            Console.Write("Please enter your amount: ");
-            var amount = Console.ReadLine();
-
-            return (currency, amount);
+            throw new Exception("Invalid email address");
         }
 
-        static (string email, string password) Login()
+        Console.Write("Please enter your password: ");
+        var password = ReadPassword();
+        if (password.Length > 4)
         {
-            Console.Write("Please enter your email: ");
-            var email = Console.ReadLine();
-            if (!Regex.IsMatch(email, "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$"))
-            {
-                throw new Exception("Invalid email address");
-            }
-
-            Console.Write("Please enter your password: ");
-            var password = ReadPassword();
-            if (password.Length > 4)
-            {
-                throw new Exception("Password must be at least 4 characters length");
-            }
-
-            return (email, password);
+            throw new Exception("Password must be at least 4 characters length");
         }
 
-        static (string name, string email, string password) Register()
+        return (email, password);
+    }
+
+    static (string name, string email, string password) Register()
+    {
+        Console.Write("Please enter your name: ");
+        var name = Console.ReadLine();
+        if (!Regex.IsMatch(name, "[a-zA-Z]"))
         {
-            Console.Write("Please enter your name: ");
-            var name = Console.ReadLine();
-            if (!Regex.IsMatch(name, "[a-zA-Z]"))
-            {
-                throw new Exception("Invalid name");
-            }
-
-            Console.Write("Please enter your email: ");
-            var email = Console.ReadLine();
-            if (!Regex.IsMatch(email, "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$"))
-            {
-                throw new Exception("Invalid email address");
-            }
-
-            Console.Write("Please enter your password: ");
-            var password = ReadPassword();
-
-            return (name, email, password);
+            throw new Exception("Invalid name");
         }
 
-        // Helper to read masked password
-        static string ReadPassword()
+        Console.Write("Please enter your email: ");
+        var email = Console.ReadLine();
+        if (!Regex.IsMatch(email, "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$"))
         {
-            var value = new List<char>();
-            while (true)
-            {
-                var key = Console.ReadKey(true);
-                Console.Write('*');
+            throw new Exception("Invalid email address");
+        }
 
-                if (key.Key == ConsoleKey.Enter)
-                {
+        Console.Write("Please enter your password: ");
+        var password = ReadPassword();
+
+        return (name, email, password);
+    }
+
+    // Helper to read masked password
+    static string ReadPassword()
+    {
+        var value = new List<char>();
+        while (true)
+        {
+            var key = Console.ReadKey(true);
+            Console.Write('*');
+
+            if (key.Key == ConsoleKey.Enter)
+            {
+                break;
+            }
+
+            value.Add(key.KeyChar);
+        }
+
+        return new string(value.ToArray());
+    }
+
+    static string AskForPassword(string email)
+    {
+        Console.Write($"Enter ({email}) password: ");
+        return ReadPassword();
+    }
+
+    static (string amount, string date, string category) AddOperation()
+    {
+        Console.Write("Please enter your amount: ");
+        var amount = Console.ReadLine();
+        Console.Write("Please enter your date: ");
+        var date = Console.ReadLine();
+        Console.Write("Choose your category: ");
+        var category = Console.ReadLine();
+
+        return (amount, date, category);
+    }
+
+    static string ChooseWallet()
+    {
+        Console.Write("Please enter your wallet name: ");
+        var name = Console.ReadLine();
+
+        return name;
+    }
+
+    static void ShowMainMenu()
+    {
+        Console.WriteLine("Choose an option");
+        Console.WriteLine("[1] Create Wallet");
+        Console.WriteLine("[2] Choose Wallet");
+        Console.WriteLine("[4] Logout");
+        Console.WriteLine();
+        Console.Write("Select an option: ");
+        // orange loop
+        do
+        {
+
+            switch (Console.ReadLine())
+            {
+                // grey loop
+                case "1":
+                    Console.WriteLine("Create Wallet Menu Selected");
+
                     break;
-                }
+                case "2":
+                    Console.WriteLine("Choose Wallet Menu Selected");
+                    // TODO: 1. ask user to choose wallet
+                    // TODO: 2. set selected wallet as Active Wallet
+                    ShowWalletMainMenu();
+                    break;
+                case "3":
+                    Console.WriteLine("Delete Wallet Menu Selected");
 
-                value.Add(key.KeyChar);
+                    break;
+
+                case "4":
+                    isAuthenticated = false;
+
+                    Console.WriteLine("Logged out successfully!");
+                    break;
             }
 
-            return new string(value.ToArray());
-        }
+        } while (isAuthenticated);
+    }
 
-        static string AskForPassword(string email)
+    static void ShowCreateWallet()
+    {
+        hasErrored = false;
+        do
         {
-            Console.Write($"Enter ({email}) password: ");
-            return ReadPassword();
-        }
-
-        static (string amount, string date, string category) AddOperation()
-        {
-            Console.Write("Please enter your amount: ");
-            var amount = Console.ReadLine();
-            Console.Write("Please enter your date: ");
-            var date = Console.ReadLine();
-            Console.Write("Choose your category: ");
-            var category = Console.ReadLine();
-
-            return (amount, date, category);
-        }
-
-        static string ChooseWallet()
-        {
-            Console.Write("Please enter your wallet name: ");
-            var name = Console.ReadLine();
-
-            return name;
-        }
-
-        static void ShowMainMenu()
-        {
-            Console.WriteLine("Choose an option");
-            Console.WriteLine("[1] Create Wallet");
-            Console.WriteLine("[2] Choose Wallet");
-            Console.WriteLine("[3] Delete Wallet");
-            Console.WriteLine("[4] Logout");
-            Console.WriteLine();
-            Console.Write("Select an option: ");
-            // orange loop
-            do
+            try
             {
+                var input = CreateWallet();
 
-                switch (Console.ReadLine())
-                {
-                    // grey loop
-                    case "1":
-                        Console.WriteLine("Create Wallet Menu Selected");
+                // TODO: 1. create wallet object
+                // TODO: 2. save wallet object to database
 
-                        break;
-                    case "2":
-                        Console.WriteLine("Choose Wallet Menu Selected");
-
-                        break;
-                    case "3":
-                        Console.WriteLine("Delete Wallet Menu Selected");
-
-                        break;
-
-                    case "4":
-                        isAuthenticated = false;
-
-                        Console.WriteLine("Logged out successfully!");
-                        break;
-                }
-
-            } while (isAuthenticated);
-        }
-
-
-        static void ShowCreateWallet()
-        {
-            hasErrored = false;
-            do
+                // TODO: 3. return to orange
+            }
+            catch (Exception exception)
             {
-                try
-                {
-                    var input = CreateWallet();
+                Console.Error.WriteLine($"Unable to create wallet: {exception.Message}");
+                Console.WriteLine();
 
-                    // TODO: 1. create wallet object
-                    // TODO: 2. save wallet object to database
+                hasErrored = true;
+            }
+        } while (hasErrored);
+    }
 
-                    // TODO: 3. return to orange
-                }
-                catch (Exception exception)
-                {
-                    Console.Error.WriteLine($"Unable to create wallet: {exception.Message}");
-                    Console.WriteLine();
+    static void ShowChooseWallet()
+    {
+        // TODO: 1. Fetch and show user wallet list by name
+        // TODO: 2. Read user selection and set it as active wallet
+        //          call Storage.SetActiveWallet
+    }
 
-                    hasErrored = true;
-                }
-            } while (hasErrored);
-        }
+    static void ShowDeleteWallet()
+    {
+        // TODO: 1. call Storage.DeleteWallet()
+        // TODO: 2. Prompt user to select new active wallet
+    }
+
+    static void LogOut()
+    {
+
+    }
+
+    static void ShowWalletMainMenu()
+    {
+        // We have an active wallet selection
+
+        Console.WriteLine("Choose an option");
+        Console.WriteLine("[1] Add Income Operation");
+        Console.WriteLine("[2] Add Expense Operation");
+        Console.WriteLine("[3] Delete Wallet");
+        Console.WriteLine("[4] Go Back");
+        Console.WriteLine("[5] Logout");
+        Console.WriteLine();
+        Console.Write("Select an option: ");
+        // orange loop
+        do
+        {
+
+            switch (Console.ReadLine())
+            {
+                // grey loop
+                case "1":
+                    Console.WriteLine("Add Operation");
+                    // TODO: 1. ask user for operation type
+                    // TODO: 2. build operation object
+                    // TODO: 3. call Storage.AddOperation
+
+                    break;
+                case "3":
+                    Console.WriteLine("Delete Wallet");
+                    // TODO: 1. call Storage.DeleteWallet
+                    // TODO: 2. return to previous menu
+                    return;
+
+                case "4":
+                    Console.WriteLine("Go Back");
+                    return;
+
+                case "5":
+                    isAuthenticated = false;
+
+                    Console.WriteLine("Logged out successfully!");
+                    break;
+            }
+
+        } while (isAuthenticated);
+    }
+
+    static Operation CreateWalletOperation()
+    {
+        // TODO: 1. ask user for category
+        // TODO: 2. ask user for operation money amount
+        // TODO: 3. build operation object
+        return null;
     }
 }
--- fall_project_2/Services/Storage.cs
+++ fall_project_2/Services/Storage.cs
@@ -1,9 +1,14 @@
+using System.Security.Cryptography;
+using System.Text;
+
 using fall_project_2.Data;
 
 namespace fall_project_2.Services;
 
-public class Storage
+public class Storage : IDisposable
 {
+    private static ApplicationDataContext _context = new DatabaseContextFactory().CreateDbContext(null);
+
     private static User s_activeUser = null;
     public static User ActiveUser
     {
@@ -17,14 +22,61 @@ public class Storage
         get => s_activeWallet ?? throw new InvalidOperationException("Active wallet is not set");
     }
 
-    private readonly ApplicationDataContext _context;
-
-    public Storage(ApplicationDataContext context)
+    public Task<User> RegisterUser(string email, string password)
     {
-        _context = context;
+
+        // TODO: 1. Hash password
+        var hasher = MD5.Create();
+        var passwordHash = Convert.ToBase64String((hasher.ComputeHash(Encoding.ASCII.GetBytes(password))));
+        hasher.Dispose();
+
+        // TODO: 2. build user object
+        // TODO: 3. save user object to database
+
+        return null;
     }
 
-    public Task<User> CreateUser(string email, string password) { return null; }
+    public Task<User> Login(string email, string password)
+    {
+        // TODO: 1. Hash password
+        var hasher = MD5.Create();
+        var passwordHash = Convert.ToBase64String((hasher.ComputeHash(Encoding.ASCII.GetBytes(password))));
+        hasher.Dispose();
 
-    public Task<User> Login(string email, string password) { return null; }
+        // TODO: 2. fetch user by email
+
+        // TODO: 3. verify fetched user passwordHash field to match with passwordHash
+
+        return null;
+    }
+
+    public Task<Wallet[]> GetUserWallets(string userEmail) { return null; }
+
+    public Task SetActiveWallet(Wallet wallet) { return null; }
+
+    public Task CreateWallet()
+    {
+        // TODO: 1. create wallet object
+        // TODO: 2. save wallet object to database
+        // TODO: 3. set as active wallet
+        return null;
+    }
+
+    public Task DeleteWallet()
+    {
+        // TODO: 1. delete active wallet
+
+        return null;
+    }
+
+    public Task AddOperation()
+    {
+        // ActiveWallet.Operations.Add();
+        return null;
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
 }
